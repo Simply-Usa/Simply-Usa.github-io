5851bbbf55916dc1fe85f9cc02ef6b288b099fe0
Simply Usa <simplyusa@gmail.com>
2019-11-18 10:37:58 +0600

    almost done

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('.filter-content').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+  });
+  $('.filter-menu ul li').click(function(){
+    $('.filter-menu ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-filter');
+    $('.filter-content').isotope({
+      filter: selector
+    })
+  });
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 20,
+    nav:true,
+    navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:3
+      }
+    }
+  });
+});
